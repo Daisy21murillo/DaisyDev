06de371b6d773427ea240532645366b746a93e7e
daisy murillo <daisy21murillo@gmail.com>
2019-02-24 17:12:56 -0700

    testing pushes

--- index.php
+++ index.php
@@ -1,18 +1,12 @@
 <!DOCTYPE html>
-<html>
-
+<html lang="en">
 <head>
-
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Daisy Dev</title>
 </head>
-<title> Daisy Dev </title>
-
 <body>
-<h1> Daisy Dev </h1>
-<p>Lorem ipsum dolor sit amet consectetur a
-dipisicing elit. Odio doloribus ullam placeat cupidit
-ate tempora, laboriosam velit quam consectetur quia magn
-i iusto fugiat ea, labore culpa earum id, ipsa corporis voluptatum? </p>
-
+    
 </body>
-
 </html>
